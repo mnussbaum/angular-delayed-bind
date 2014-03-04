d056f07b215cd27f368131275755a3649c417144
mnussbaum <michaelnussbaum08@gmail.com>
2014-03-03 21:46:59 -0600

    Initial commit

---
+++ src/delayed_bind.js
@@ -0,0 +1,23 @@
+angular.module("delayed-bind", []).
+  directive("delayedBind", ["$timeout", function($timeout) {
+    return {
+      restrict: "A",
+      scope: true,
+      compile: function(element) {
+        element.text("{{delayedBind}}");
+
+        return function(scope, element, attr) {
+          scope.$watch(
+            function() { return scope.$eval(attr.delayedBind); },
+            function(newValue, oldValue) {
+              if(newValue === oldValue) {
+                scope.delayedBind = newValue;
+              } else {
+                $timeout(function() { scope.delayedBind = newValue; }, 1000);
+              }
+            }
+          );
+        };
+      }
+    };
+  }]);
---
+++ test/delayed_bind_spec.js
@@ -0,0 +1,37 @@
+"use strict";
+
+describe("delayedBind", function() {
+  var scope,
+      element,
+      $compile,
+      compiled,
+      $timeout;
+
+  beforeEach(module("delayed-bind"));
+  beforeEach(inject(function(_$compile_, $rootScope, _$timeout_) {
+    $compile = _$compile_;
+    scope = $rootScope.$new();
+    $timeout = _$timeout_;
+  }));
+
+  it("sets the evaled, passed in value as the text of the element", function() {
+      scope.woo = "test";
+      compiled = $compile("<div delayed-bind='woo'></div>");
+      element = compiled(scope);
+      scope.$digest();
+
+      expect(element.text()).toBe("test");
+  });
+
+  it("updates the value after a timeout", function() {
+      scope.woo = "test";
+      compiled = $compile("<div delayed-bind='woo'></div>");
+      element = compiled(scope);
+      scope.$digest();
+      scope.woo = "fubar";
+      scope.$digest();
+      $timeout.flush();
+      expect(element.text()).toBe("fubar");
+      $timeout.verifyNoPendingTasks();
+  });
+});
